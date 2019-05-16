e4fd0447d2f6e8259a7954fafcda5515ed465c33
javad <javad@203.229.203.100>
2019-05-16 14:12:25 +0900

    Gugudan 7단과 8단

---
+++ Gugudan2/src/Gugudan2.java
@@ -0,0 +1,15 @@
+
+public class Gugudan2 {
+		public static void main(String[] args) {
+			//6´Ü
+			int i = 1;
+			while(i < 10) {
+				System.out.println(6 * i);
+				i = i + 1;
+			}
+			// 7´Ü
+			for(int j = 1; j < 10; j++) {
+				System.out.println(7 * j);
+			}
+		}
+}
